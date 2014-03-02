1c24bc96b1d537f9ef454052305f0fce92d34f37
Darryl Woods <darryl.woods@viu.ca>
2014-03-02 07:56:48 -0800

    Added red numbered markers for villages

--- geonode/mediastory/static/mediastory/js/mediastory.js
+++ geonode/mediastory/static/mediastory/js/mediastory.js
@@ -2,6 +2,38 @@
  * Created by dwoods on 2014-03-01.
  */
 
+var NumberedDivIcon = L.Icon.extend({
+	options: {
+    // EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
+    iconUrl: '/static/mediastory/images/red-circle-icon.png',
+    number: '',
+    shadowUrl: null,
+    iconSize: new L.Point(25, 25),
+		iconAnchor: new L.Point(13, 12),
+		popupAnchor: new L.Point(0, -15),
+		className: 'leaflet-div-icon'
+	},
+
+	createIcon: function () {
+		var div = document.createElement('div');
+		var img = this._createImg(this.options['iconUrl']);
+		var numdiv = document.createElement('div');
+		numdiv.setAttribute ( "class", "number" );
+		numdiv.innerHTML = this.options['number'] || '';
+		div.appendChild ( img );
+		div.appendChild ( numdiv );
+		this._setIconStyles(div, 'icon');
+		return div;
+	},
+
+	//you could change this to add a shadow like in the normal marker if you really wanted
+	createShadow: function () {
+		return null;
+	}
+});
+
+
+
 var CustomMarker = L.Marker.extend({
 
     bindPopup: function(htmlContent, options) {
