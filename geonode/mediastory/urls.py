b9ec5689d9d1eedf6eb87d9540c6eb165bb844ed
Darryl Woods <darryl.woods@viu.ca>
2014-05-05 23:25:31 -0700

    Added /mediastory/ as URL for main map

--- geonode/mediastory/urls.py
+++ geonode/mediastory/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 from .views import MediaItemList, LocationList, MediaItemDetail
 
 urlpatterns = patterns('',
-    url(r'^locations/$', LocationList.as_view(), name='location_list'),
+    url(r'^$', LocationList.as_view(), name='location_list'),
+    url(r'^locations/$', LocationList.as_view(), name='location_list_alt'),
     url(r'^location_media/(\d+)/$', MediaItemList.as_view(), name='mediaitem_list'),
     url(r'^media/(?P<pk>\d+)/$', MediaItemDetail.as_view(), name='mediaitem_detail'),
 )
