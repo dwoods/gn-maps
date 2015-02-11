5a44d026368f4b1ef70aaade702e2e29674d3f91
Darryl Woods <darryl.woods@viu.ca>
2015-02-11 01:15:38 -0800

    Removed attribution control

--- geonode/mediastory/static/mediastory/js/app.js
+++ geonode/mediastory/static/mediastory/js/app.js
@@ -263,12 +263,12 @@ map.on("layerremove", updateAttribution);
 var attributionControl = L.control({
   position: "bottomright"
 });
-attributionControl.onAdd = function (map) {
-  var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
-  return div;
-};
-map.addControl(attributionControl);
+//attributionControl.onAdd = function (map) {
+//  var div = L.DomUtil.create("div", "leaflet-control-attribution");
+//  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+//  return div;
+//};
+//map.addControl(attributionControl);
 
 var zoomControl = L.control.zoom({
   position: "bottomright"
